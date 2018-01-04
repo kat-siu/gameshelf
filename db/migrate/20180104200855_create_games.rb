180cc90ce53c58bd4a136997e021bb80a36427db
k.siu92@gmail.com <k.siu92@gmail.com>
2018-01-04 15:14:44 -0500

    Making game model

---
+++ db/migrate/20180104200855_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :games do |t|
+      t.integer :gameshelf_id, null: false
+      t.string :title, null: false
+      t.text :description, null: false
+      t.string :cover_img_url, null: false
+
+      t.timestamps
+    end
+  end
+end
