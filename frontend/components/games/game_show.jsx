5b386b62415894bedb9d03d328cc88995dcb6b86
k.siu92@gmail.com <k.siu92@gmail.com>
2018-01-11 23:02:16 -0500

    Removed lots of commented out code

--- frontend/components/games/game_show.jsx
+++ frontend/components/games/game_show.jsx
@@ -54,56 +54,6 @@ class GameShow extends React.Component {
     } else {
 
       const default_shelves = ["Played", "Currently Playing", "Want to Play"];
-      //
-      // const status_dropdown = this.props.gameshelves.map((gameshelf) => {
-      //   if ( default_shelves.includes(gameshelf.title) {
-      //     return (
-      //       <section>
-      //         <option key={gameshelf.id} value={gameshelf.id}>{gameshelf.title}</option>
-      //         <button onClick={this.addToShelf} value={gameshelf.id} className="styled-btn">Add to a gameshelf</button>
-      //         <button onClick={this.removeFromShelf} value={gameshelf.id} className="styled-btn">Remove from shelf</button>
-      //       </section>
-      //     )
-      //   })
-      // })
-//       const play_status =
-//           if ( default_shelves.includes(this.props.gameshelves.title)) {
-//             return (
-//               <div>
-//                 Play Status:
-//                 <select onChange={this.addToShelf} name="status">
-//                   <button class="btn btn-primary dropdown-toggle" id="menu1" type="button" data-toggle="dropdown">Dropdown Example
-//                   <option key={gameshelf.id} value="{gameshelf.id}">Currently Playing</option>
-//                   <li role="presentation"><a role="menuitem" tabindex="-1" href="#">HTML</a></li>
-//
-//                 </select>
-//               </div>
-//               </label>
-//             )
-//           }
-//         })
-  //
-  // renderPlayStatus() {
-  //   const playStatusShelves = this.props.gameshelves.filter((gameshelf) => default_shelves.includes(default_shelves));
-  //   return (
-  //     <select onChange={this.addToShelf}>
-  //       {playStatusShelves.map((shelf, idx) => )}
-  //     </select>
-  //   )
-  //
-  //   shelves.filter( (shelf) => shelf.user_id === this.props.currentUser.id );
-  //   return (
-  //     <ul className="shelvings-display">
-  //       {userShelves.map((shelf, i) =>
-  //         <li className="shelving-display" key={i}>
-  //           <span id="shelving-display" onClick={this.bookshelfLink(shelf)}>
-  //             {shelf.name}
-  //           </span>
-  //           <i id="delete-shelving-book-show" className="fa fa-times fa-5" onClick={this.handleDeleteShelving(shelf)}></i>
-  //         </li>)}
-  //     </ul>
-  //   );
-  // }
 
     // const shelf_buttons = this.props.gameshelves.map((gameshelf) => {
     //   debugger
@@ -119,8 +69,8 @@ class GameShow extends React.Component {
     const shelf_options = this.props.gameshelves.map((gameshelf) => {
       debugger
       if (default_shelves.includes(gameshelf.title)) {
-        var selected = false;
-        for (var i = 0; i < gameshelf.games.length; i++) {
+        let selected = false;
+        for (let i = 0; i < gameshelf.games.length; i++) {
           if (gameshelf.games[i].id == this.props.game.id) {
             selected = true;
           }
@@ -132,36 +82,6 @@ class GameShow extends React.Component {
         }
       }
     })
-
-
-
-
-    // const shelf_options = this.props.gameshelves.map((gameshelf) => {
-    //   if ( default_shelves.includes(gameshelf.title) ) {
-    //     if ( gameshelf.games.includes(this.props.game) ) {
-    //       <option key={gameshelf.id} value={gameshelf.id} selected>{gameshelf.title}</option>
-    //     } else {
-    //       return (
-    //         <option key={gameshelf.id} value={gameshelf.id}>{gameshelf.title}</option>
-    //       )
-    //     }
-    //   }
-    // })
-
-    // <div class="btn-group">
-    //   <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    //     Action
-    //   </button>
-    //   <div class="dropdown-menu">
-    //     <a class="dropdown-item" href="#">Action</a>
-    //     <a class="dropdown-item" href="#">Another action</a>
-    //     <a class="dropdown-item" href="#">Something else here</a>
-    //     <div class="dropdown-divider"></div>
-    //     <a class="dropdown-item" href="#">Separated link</a>
-    //   </div>
-    // </div>
-
-
 
     return (
       <div className="game-show-box">
@@ -177,19 +97,19 @@ class GameShow extends React.Component {
             <br />
 
               <div className="status-dropdown">
-                <label>Play Status:
+                <p className="uppercase"><font color="#00afcc">Play Status: </font></p>
                   <select onChange={this.addToShelf} name="status">
                     <option disabled>Select Status</option>
                     {shelf_options}
                   </select>
-                </label>
               </div>
 
-
+              <br /><br />
             <div>
               <p className="uppercase"><font color="#00afcc">Platform:</font></p>
               {this.props.game.platform}
             </div>
+
             <br /> <br />
             <div>
               <p className="uppercase"><font color="#00afcc">Description:</font></p>
@@ -197,9 +117,7 @@ class GameShow extends React.Component {
             </div>
           </div>
         </div>
-
         <ReviewIndexContainer className="review-form" gameId={this.props.game.id} />
-
       </div>
     )
     }
